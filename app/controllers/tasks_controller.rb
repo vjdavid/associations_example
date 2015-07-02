cf0d12299e7b56ca1310c3359697aac220fda628
vjdavid <vjdavid@gmail.com>
2015-07-02 14:33:00 -0700

    added and finished tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :get_people]
+  before_action :get_person, only: [:get_people]
+  before_action :find_person, only: [:update_people, :patch_update_people]
+
+  def get_people
+    render json: @get_all
+  end
+
+  def show
+    render json: @task
+  end
+
+  def update_people
+    if @person.update(person_params)
+      render json: @person
+    else
+      render json: @person.errors
+    end
+  end
+
+  def patch_update_people
+    if @person.update(person_params)
+      render json: @person
+    else
+      render json: @person.errors
+    end
+  end
+
+  private
+
+  def person_params
+    params.permit(:name)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def find_person
+    @person = Person.find(params[:id])
+  end
+
+  def get_person
+    @get_all = @task.people
+  end
+
 end
