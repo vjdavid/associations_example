b8531b3f0525b0416f031ff1de504abf498fef81
vjdavid <vjdavid@gmail.com>
2015-07-01 18:05:53 -0700

    created routes for people tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
 
  resources :people do
    member do
-     put 'tasks', to: 'tasks#update_tasks'
-     get 'tasks', to: 'tasks#get_tasks'
-     patch 'tasks', to: 'tasks#updates_tasks'
+     put 'tasks/:id', to: 'people#update_tasks'
+     get 'tasks', to: 'people#get_tasks'
+     patch 'tasks/:id', to: 'people#patch_update_tasks'
    end
  end
 
  resources :tasks do
    member do
-     put 'people', to: 'people#update_people'
-     get 'people', to: 'people#get_people'
-     patch 'people', to: 'people#patch_people'
+     put 'people/:id', to: 'tasks#update_people'
+     get 'people', to: 'tasks#get_people'
+     patch 'people/:id', to: 'tasks#patch_update_people'
    end
  end
 
