e4e7b73b68ac49f226b5d84ab8a0986f629c582d
vjdavid <vjdavid@gmail.com>
2015-07-01 18:04:45 -0700

    created actions show, get_tasks and index for people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,34 @@
 class PeopleController < ApplicationController
+
+  before_action :find_person, only: [:show, :get_tasks]
+  before_action :find_tasks, only: [:get_tasks]
+
   def index
     @people = Person.all
 
     render json: @people
   end
+
+  def get_tasks
+    render json: @tasks
+  end
+
+  def show
+    render json: @person
+  end
+
+  private
+
+  def person_params
+    params.permit(:name)
+  end
+
+  def find_person
+    @person = Person.find(params[:id])
+  end
+
+  def find_tasks
+    @tasks = @person.tasks
+  end
+
 end
