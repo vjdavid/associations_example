288ee6e76e888a75ff0270c4e879da02bd7a8f2f
vjdavid <vjdavid621@gmail.com>
2015-07-03 00:58:40 -0700

    added relationship between person task and status

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
-  has_and_belongs_to_many :tasks
+  has_many :statuses
+  has_many :tasks, through: :statuses
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
-  has_and_belongs_to_many :people
+  has_many :statuses
+  has_many :people, through: :statuses
 end
